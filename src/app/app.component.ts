94acc5cfcbd130394316c89522baff99ac58811e
anastminsk <anast-minsk@yandex.ru>
2018-09-03 16:39:15 +0300

    feature: split into two components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,52 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `<h1>{{title}}</h1>
-    <p>There are {{totalCarParts()}} total parts in stock.</p>
-    <ul>
-      <li *ngFor="let carPart of carParts">
-        <h2>{{carPart.name | uppercase}}</h2>
-        <p>{{carPart.description}}</p>
-        <p>{{carPart.price | currency:'EUR':true}}</p>
-        <p *ngIf="carPart.inStock > 0">{{carPart.inStock}} in Stock</p>
-        <p *ngIf="carPart.inStock === 0">Out of Stock</p>
-      </li>
-    </ul>`,
+    <car-parts></car-parts>`,
   //templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'Ultra Racing';
-  carParts = [{
-    "id": 1,
-    "name": "Super Tires",
-    "description": "These tires are the best",
-    "inStock": 5,
-    "price": 4.99
-  },
-  {
-    "id": 2,
-    "name": "Reinforced Shocks",
-    "description": "Shocks made from kryptonite",
-    "inStock": 4,
-    "price": 9.99
-  },
-  {
-    "id": 3,
-    "name": "Padded Seats",
-    "description": "Super soft seats for a smooth ride",
-    "inStock": 0,
-    "price": 24.99
-  }];
-
-  /*totalCarParts() {
-    let sum = 0;
-    for (let carPart of this.carParts) {
-      sum += carPart.inStock;
-    }
-    return sum;
-  }*/
-
-  totalCarParts() {
-    return this.carParts.reduce((prev, current) => prev + current.inStock, 0);
-  }
 }
---
+++ src/components/car-parts/car-parts.component.ts
@@ -0,0 +1,45 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'car-parts',
+  template: `
+    <p>There are {{totalCarParts()}} total parts in stock.</p>
+    <ul>
+      <li *ngFor="let carPart of carParts">
+        <h2>{{carPart.name | uppercase}}</h2>
+        <p>{{carPart.description}}</p>
+        <p>{{carPart.price | currency:'EUR':true}}</p>
+        <p *ngIf="carPart.inStock > 0">{{carPart.inStock}} in Stock</p>
+        <p *ngIf="carPart.inStock === 0">Out of Stock</p>
+      </li>
+    </ul>`,
+  //styleUrls: ['./components.car-parts.css']
+})
+
+export class CarPartsComponent {
+  carParts = [{
+    "id": 1,
+    "name": "Super Tires",
+    "description": "These tires are the best",
+    "inStock": 5,
+    "price": 4.99
+  },
+  {
+    "id": 2,
+    "name": "Reinforced Shocks",
+    "description": "Shocks made from kryptonite",
+    "inStock": 4,
+    "price": 9.99
+  },
+  {
+    "id": 3,
+    "name": "Padded Seats",
+    "description": "Super soft seats for a smooth ride",
+    "inStock": 0,
+    "price": 24.99
+  }];
+
+  totalCarParts() {
+    return this.carParts.reduce((prev, current) => prev + current.inStock, 0);
+  }
+}
