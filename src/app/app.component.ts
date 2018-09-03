e85931a7e7b3553e592b9acbdb0144e62a1cc961
anastminsk <anast-minsk@yandex.ru>
2018-09-03 15:38:51 +0300

    feature: add pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,8 +5,9 @@ import { Component } from '@angular/core';
   template: `<h1>{{title}}</h1>
     <ul>
       <li *ngFor="let carPart of carParts">
-        <h2>{{carPart.name}}</h2>
+        <h2>{{carPart.name | uppercase}}</h2>
         <p>{{carPart.description}}</p>
+        <p>{{carPart.price | currency:'EUR':true}}</p>
         <p *ngIf="carPart.inStock > 0">{{carPart.inStock}} in Stock</p>
         <p *ngIf="carPart.inStock === 0">Out of Stock</p>
       </li>
@@ -21,18 +22,21 @@ export class AppComponent {
     "id": 1,
     "name": "Super Tires",
     "description": "These tires are the best",
-    "inStock": 5
+    "inStock": 5,
+    "price": 4.99
   },
   {
     "id": 2,
     "name": "Reinforced Shocks",
     "description": "Shocks made from kryptonite",
-    "inStock": 4
+    "inStock": 4,
+    "price": 9.99
   },
   {
     "id": 3,
     "name": "Padded Seats",
     "description": "Super soft seats for a smooth ride",
-    "inStock": 0
+    "inStock": 0,
+    "price": 24.99
   }];
 }
