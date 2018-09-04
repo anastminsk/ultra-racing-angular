5be5615a88e9f7e0bc6330ae31c8e56671777707
anastminsk <anast-minsk@yandex.ru>
2018-09-04 22:17:40 +0300

    feature: change html structure and add property binding

--- src/app/car-parts/mocks.ts
+++ src/app/car-parts/mocks.ts
@@ -5,19 +5,22 @@ export const CARPARTS: CarPart[] = [{
   "name": "Super Tires",
   "description": "These tires are the best",
   "inStock": 5,
-  "price": 4.99
+  "price": 4.99,
+  "image": "../assets/img/tire.jpg"
 },
 {
   "id": 2,
   "name": "Reinforced Shocks",
   "description": "Shocks made from kryptonite",
   "inStock": 4,
-  "price": 9.99
+  "price": 9.99,
+  "image": "../assets/img/shocks.jpg"
 },
 {
   "id": 3,
   "name": "Padded Seats",
   "description": "Super soft seats for a smooth ride",
   "inStock": 0,
-  "price": 24.99
+  "price": 24.99,
+  "image": "../assets/img/seat.jpg"
 }];
