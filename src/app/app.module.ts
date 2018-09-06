53b869201ace3af68c873b8a1a5da126e13e66ad
anastminsk <anast-minsk@yandex.ru>
2018-09-06 08:47:07 +0300

    feature: use ngModel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
@@ -10,7 +11,8 @@ import { CarPartsComponent } from './car-parts/car-parts.component';
     CarPartsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
